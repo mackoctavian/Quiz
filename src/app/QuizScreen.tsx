831da7209fa83b572ff9254c0701d01a8a4614eb
Mack Octavian <maestrooctavian@gmail.com>
2025-01-20 00:48:55 +0300

    Added lottie animation at the end of the quiz

--- src/app/QuizScreen.tsx
+++ src/app/QuizScreen.tsx
@@ -8,6 +8,8 @@ import CustomButton from "../components/CustomButton";
 import { useQuizContext } from "../providers/QuizProvider";
 import { useEffect, useRef, useState } from "react";
 import { useTimer } from "../hooks/useTimer";
+import party from "../../assets/party.json";
+import LottieView from "lottie-react-native";
 
 export default function QuizScreen() {
   const { question, questionIndex, onNext, score, totalQuestion, bestScore } =
@@ -41,12 +43,19 @@ export default function QuizScreen() {
             <Text style={styles.time}>{time} sec</Text>
           </>
         ) : (
-          <Card title="well done">
-            <Text>
-              Correct answers: {score}/{totalQuestion}
-            </Text>
-            <Text>Best score: {bestScore}</Text>
-          </Card>
+          <>
+            <LottieView
+              style={StyleSheet.absoluteFill}
+              autoPlay
+              source={require("../../assets/party.json")}
+            />
+            <Card title="well done">
+              <Text>
+                Correct answers: {score}/{totalQuestion}
+              </Text>
+              <Text>Best score: {bestScore}</Text>
+            </Card>
+          </>
         )}
 
         {/* Footer */}
